65a80d7f19b481306927a4cac3684c2a8cc0bcb8
Dolores Pavon <dolorespavon01@gmail.com>
2024-06-18 20:26:37 -0300

    Refactor - Download of data from IMDB & upload to MongoDB

---
+++ imdb_data_thread.jl
@@ -0,0 +1,127 @@
+using HTTP
+using CodecZlib
+using Dates
+include("mongo_connection_threads.jl")
+
+hora_start_script = now()
+println("Fecha y hora actual: ", hora_start_script)
+
+
+function getDataFromURLFile(imdb_file::String)
+
+    i = 0
+    while i < 5
+        try
+            response = HTTP.request("GET", imdb_file)
+            body = response.body
+            filehandler = GzipDecompressorStream(IOBuffer(body))
+
+            return filehandler
+        catch e
+            println(e)
+            i += 1
+        end
+    end
+
+end
+
+function proccess_data(data::Array, header::Array)
+
+    subdata_imdb = Dict()
+    for (key, value) in zip(header, data)
+        try
+            value = parse(Float64, value)
+        catch
+        end
+        subdata_imdb[key] = value
+    end
+
+    return subdata_imdb
+
+end
+
+function get_imdb_payload(imdb_file::String, callback::Function)
+
+    imdb_data = getDataFromURLFile(imdb_file)
+    filename_collection = split(imdb_file, "/")
+    filename_collection = replace(filename_collection[length(filename_collection)], ".tsv.gz" => "")
+    filename_collection = "TEST____" * replace(filename_collection, "." => "_")
+
+    index_header = 1
+    header = []
+
+    counter = 0
+    limit = 10000
+    len = 0
+    imdb_payload = Dict[]
+
+    println("Normalize  ", filename_collection)
+
+    for linea in eachline(imdb_data)
+        linea = split(linea, "\t")
+        if index_header == 1
+            header = linea
+            index_header = 0
+        elseif counter >= limit
+            len = len + counter
+            print("Insertando en $(filename_collection)   ::::::  Cantidad de Elementos Insertados al momento: $(len)\r")
+            callback(filename_collection, imdb_payload)
+            imdb_payload = Dict[]
+            counter = 0
+        end
+        push!(imdb_payload, proccess_data(linea, header))
+
+        counter = counter + 1
+    end
+
+    if counter >= 0
+        len += counter
+        println("Insertando en $(filename_collection)   ::::::  Cantidad de Elementos Insertados al momento: $(len)")
+        callback(filename_collection, imdb_payload)
+    end
+end
+
+function proccess_imdb_files(imdb_files::Vector{String}, callback::Function)
+
+    hour_func_start = now()
+
+    for imdb_file in imdb_files
+        println("Extract From ::::::  ", imdb_file)
+        get_imdb_payload(imdb_file, callback)
+        println("Tardanza del archivo $imdb_file ::  ", ((now() -  hour_func_start) / Millisecond(1)) / 60000)
+        hour_func_start = now()
+
+        println()
+        println()
+    end
+    println("Tardanza total ::  ", ((now() - hora_start_script) / Millisecond(1)) / 60000)
+end
+
+imdb_files = [
+    "https://datasets.imdbws.com/title.ratings.tsv.gz",
+    "https://datasets.imdbws.com/title.episode.tsv.gz",
+    "https://datasets.imdbws.com/title.basics.tsv.gz",
+    "https://datasets.imdbws.com/title.crew.tsv.gz",
+    "https://datasets.imdbws.com/name.basics.tsv.gz",
+    "https://datasets.imdbws.com/title.akas.tsv.gz",
+    "https://datasets.imdbws.com/title.principals.tsv.gz"
+]
+
+
+# Mongo connection
+
+function upload_database(host::String, port::Int, database::String)
+    return function (collection::String, contents::Array)
+        insert_many_contents(host, port, database, collection, contents)
+    end
+end
+
+try
+    host = "localhost"
+    port = 27017
+    database = "TEST___imdb_data"
+    proccess_imdb_files(imdb_files, upload_database(host, port, database))
+
+catch error
+    println(error)
+end
---
+++ mongo_connection_threads.jl
@@ -0,0 +1,183 @@
+import Mongoc: Client, BSON, as_dict, find, find_one, insert_one, insert_many, aggregate, find_and_modify
+import Base.Threads.@threads
+
+struct Mongo
+    connection::Client
+end
+
+function Mongo(host::String, port::Int)::Mongo
+    try
+        mongo_connection = Client(host, port)
+        return Mongo(mongo_connection)
+    catch e
+        println("Error while creating Mongo instance.")
+        rethrow(e)
+    end
+end
+
+function find_content(mongo::Mongo, database::String, collection::String, query::Dict=Dict(), projection::Dict=Dict(), sort::Dict=Dict())::Dict
+    try
+        mongo_collection = mongo.connection[database][collection]
+        query_bson = BSON(query)
+        options_dict = Dict("projection" => projection, "sort" => sort)
+        options_bson = BSON(options_dict)
+        result = find_one(mongo_collection, query_bson, options=options_bson)
+        if !isnothing(result)
+            result = as_dict(result)
+        end
+        return result
+    catch e
+        println("Error while getting element: $e")
+        result = nothing
+    end
+    return result
+end
+
+function find_many_contents(mongo::Mongo, database::String, collection::String, query::Dict=Dict(), projection::Dict=Dict(), sort::Dict=Dict())::Array
+    try
+        mongo_collection = mongo.connection[database][collection]
+        query_bson = BSON(query)
+        options_dict = Dict("projection" => projection, "sort" => sort)
+        options_bson = BSON(options_dict)
+        result = find(mongo_collection, query_bson, options=options_bson)
+        if !isnothing(result)
+            result_array = Dict[]
+            for item in result
+                push!(result_array, as_dict(item))
+            end
+            result = result_array
+        end
+        return result
+    catch e
+        println("Error while getting many elements: $e")
+        result = nothing
+    end
+    return result
+end
+
+function insert_content(mongo::Mongo, database::String, collection::String, content::Dict)
+    try
+        mongo_collection = mongo.connection[database][collection]
+        bson_content = BSON(content)
+        insert_one(mongo_collection, bson_content)
+    catch e
+        rethrow("Error inserting content: $e")
+    end
+end
+
+
+function insert_contents_without_connection(contents, host, port, database, collection)
+    mongo = Mongo(host, port)
+    mongo_collection = mongo.connection[database][collection]
+    documents = BSON[]
+    for content in contents
+        push!(documents, BSON(content))
+    end
+    insert_many(mongo_collection, documents)
+    return documents
+end
+
+function insert_many_contents(mongo::Mongo, database::String, collection::String, contents::Array)
+    try
+        mongo_collection = mongo.connection[database][collection]
+        documents = BSON[]
+        for content in contents
+            push!(documents, BSON(content))
+        end
+        insert_many(mongo_collection, documents)
+    catch e
+        rethrow("Error inserting content: $e")
+    end
+end
+
+function insert_many_contents(host::String, port::Int, database::String, collection::String, contents::Array)
+    try
+
+        chunks = Iterators.partition(contents, length(contents) ÷ Threads.nthreads())
+        tasks = map(chunks) do chunk
+            Threads.@spawn insert_contents_without_connection(chunk, host, port, database, collection)
+        end
+        chunk_sums = fetch.(tasks)
+        return chunk_sums
+
+
+    catch e
+        rethrow("Error inserting content: $e")
+    end
+end
+
+function run_aggregate(mongo::Mongo, database::String, collection::String, pipeline::Array)::Array
+    try
+        mongo_collection = mongo.connection[database][collection]
+        pipeline_bson = BSON(pipeline)
+        result = aggregate(mongo_collection, pipeline_bson)
+        if !isnothing(result)
+            docs = Dict[]
+            for item in result
+                push!(docs, as_dict(item))
+            end
+            result = docs
+        end
+        return result
+    catch e
+        rethrow("Error while running aggregate: $e")
+    end
+end
+
+function update_contents(mongo::Mongo, database::String, collection::String, query::Dict, updates::Dict)
+    try
+        mongo_collection = mongo.connection[database][collection]
+        bson_query = BSON(query)
+        bson_updates = BSON(updates)
+        find_and_modify(mongo_collection, bson_query, update=bson_updates)
+    catch e
+        rethrow("Error while updating element in '$database.$collection': $e")
+    end
+end
+
+#= USE
+
+include("mongo_connection.jl") and use all module functions.
+
+Create Mongo connection setting host and port:
+host = "localhost"
+port = 27017
+mongo = Mongo(host, port)
+
+query = Dict( "imdbId" => "tt0075314" )
+projection = Dict( "_id" => 0, "averageRating" => 1, "imdbId" => 1 )
+sort = Dict( "averageRating" => -1 )
+
+find_one:
+content = find_contents(mongo, db, col, query, projection, sort)
+
+find_many:
+contents = find_many_contents(mongo, db, col, query, projection, sort)
+
+insert_one:
+doc = Dict( "name" => "David", "age" => 15 )
+insert_many_contents(mongo, db, col, docs)
+
+insert_many:
+docs = Dict[
+    Dict( "name" => "David", "age" => 20 ),
+    Dict( "name" => "Ricardo", "age" => 35 ),
+    Dict( "name" => "Lucas", "age" => 54 )
+]
+insert_many_contents(mongo, db, col, docs)
+
+aggregate:
+pipeline = Dict[
+    Dict( "\$match" => Dict( "startYear" => Dict( "\$gt" => "2015" )  ) ),
+    Dict( "\$group" => Dict( "_id" => "\$startYear", "count" => Dict( "\$sum" => 1 ) ) ),
+    Dict( "\$project" => Dict( "_id" => 0, "year" => "\$_id", "count" => 1 ) ),
+    Dict( "\$sort" => Dict( "year" => -1 ) ),
+]
+result = run_aggregate(mongo, "testing", "imdbTitleBasics", pipeline)
+
+update:
+query = Dict( "imdbId" => "tt0103781" )
+update = Dict( "\$set" => Dict( "genres" => "Short" ) )
+update_contents( mongo, db, col, query, update )
+
+=#
