407d62287490b4da986a7575c3e148240f4d21dc
Dolores Pavon <dolorespavon01@gmail.com>
2024-06-28 21:55:17 -0300

    Code Refactoring: Organization and Cleanup

---
+++ extract_data/letterbox.jl
@@ -0,0 +1,303 @@
+using Dates
+import HTTP
+import Formatting
+import Base.Threads.@threads
+include("../BBDD/mongo_connection.jl")
+
+using PyCall
+pyimport_conda("bs4", "beautifulsoup4")
+bs4 = pyimport("bs4").BeautifulSoup
+
+
+base_url = "https://letterboxd.com/films/ajax/popular/this/week/size/small/page/{}/?esiAllowFilters=true"
+content_type = "movie"
+
+
+function process_scraping(content_type::String, url::String)
+    page_step = 2
+    n_pages = get_page_numbers(url)
+    n_pages = 10
+    println("n_pages :: $n_pages")
+
+    for i in 1:page_step:n_pages
+        println([x for x in i:(i+page_step)])
+        crawlers(url, [x for x in i:(i+page_step)], content_type)
+    end
+
+end
+
+
+function crawlers(url::String, pages_range, content_type::String)::Array
+    
+    chunks = Iterators.partition(pages_range, length(pages_range) ÷ Threads.nthreads())
+    tasks = map(chunks) do chunk
+        # Threads.@spawn proccess_crawlers(url, chunk, content_type)
+        proccess_crawlers(url, chunk, content_type) # No using threads
+        
+    end
+    
+    fetch.(tasks) 
+end
+
+
+function proccess_crawlers(url::String, pages_range, content_type::String)
+    for n_page in pages_range
+        url =  Formatting.format(url, string(n_page))
+        crawlers_result = get_crawlers(url)
+        println("\nPAGE $n_page | $(length(crawlers_result)) items")
+        if isnothing(crawlers_result) || length(crawlers_result)==0 continue end
+        crawlers_item_result = Dict[]
+        for item in crawlers_result
+            item_result = get_scraping(item, content_type)
+            if isnothing(item_result) continue end
+            push!(crawlers_item_result, item_result)
+        end
+        insert(crawlers_item_result)
+    end
+end
+
+
+function get_crawlers(url::String)
+    host = "https://letterboxd.com"
+    list_crawlers = Dict[]
+    html = get_html(url)
+    if isnothing(html)
+        return
+    end
+
+    items = html.find_all("li", Dict( "class" => "listitem"))
+
+    if isnothing(items)
+        return []
+    end 
+    for item in items
+        div_info = item.div
+        if isnothing(div_info)
+            continue
+        end
+        relative_link = div_info.get("data-target-link")
+        content_id = div_info.get("data-film-id")
+
+        if isnothing(relative_link) || isnothing(content_id)
+            continue
+        end
+
+        link = host * relative_link
+        rating = "";
+        try
+            rating = parse(Float64, item.get("data-average-rating"))
+        catch
+            rating = ""
+        end 
+
+        item_to_scrape = Dict(
+            "url" => link,
+            "id" => content_id,
+            "rating" => rating
+        )
+        push!(list_crawlers, item_to_scrape)
+    end
+
+    return list_crawlers
+end
+
+
+function get_scraping(item, content_type::String)
+
+    scraped_item = nothing;
+
+    id = item["id"]
+    url = item["url"]
+    rating = item["rating"]
+
+    html = get_html(url)
+
+    if isnothing(html)
+        return
+    end 
+    try
+        section_header = html.find("section", Dict( "class" => "film-header-group"))
+
+        title = nothing;
+        try
+            h1_title = section_header.find("h1")
+            title = strip(h1_title.text)
+        catch e
+            print("\nERROR:\n$e\n")
+        end
+
+        imdb_id, tmdb_id = nothing, nothing;
+        try
+            a_tag_imdb = html.find("a", Dict("data-track-action" => "IMDb"))
+            if !isnothing(a_tag_imdb) && !isnothing(a_tag_imdb.get("href"))
+                imdb_link = a_tag_imdb.get("href")
+                reg_match = match(r"tt\d+", imdb_link)
+                if !isnothing(reg_match)
+                    imdb_id = reg_match.match
+                end
+            end
+            a_tag_tmdb = html.find("a", Dict("data-track-action" => "TMDb"))
+            if !isnothing(a_tag_tmdb) && !isnothing(a_tag_tmdb.get("href"))
+                tmdb_link = a_tag_tmdb.get("href")
+                tmdb_id =  split( tmdb_link, "/" )[ end-1 ] # tmdb_link.split("/")[-2]
+            end
+        catch e
+            print("\nERROR:\n$e\n")
+        end
+
+        comments = nothing;
+        try
+            comments = get_comments(url)
+        catch e
+            print("\nERROR COMMENTS:\n$e\n")
+        end
+
+        scraped_item = Dict(
+            "platformCode" => "letterbox",
+            "id" => id,
+            "permalink" => url,
+            "title" => title,
+            "type" => content_type,
+            
+            "comments" => comments,
+
+            "imdb_id" => imdb_id,
+            "tmdb_id" => tmdb_id,
+            
+            "popularity" => Dict(
+                # "votes" => votes,
+                # "likes" => likes,
+                # "views" => views,
+                "rating" => rating,
+            ),
+            
+            "createdAt" => string(today()),
+        )
+
+    catch e
+        print("\nERROR WHILE SCRAPING ITEM:\n$e\n")
+    end
+
+    # print("\nSCRAPED ITEM:\n$scraped_item\n")
+
+    return scraped_item
+end
+
+
+function get_page_numbers(url::String)::Int
+
+    url = Formatting.format(url, string(1))
+    println(url)
+    html = get_html(url)
+    if isnothing(html)
+        return 0
+    end
+
+    movies_raw_number = html.find("p", class_="ui-block-heading").text
+
+    total_movies = parse(Int,filter(x -> isdigit(x), movies_raw_number))
+            
+    movies = html.find_all("li", Dict("class" => "listitem"))
+
+    total_pages = round(Int,total_movies / length(movies))
+
+    return total_pages
+
+end
+
+
+function get_comments(url::String)::Array
+    movie_reviews = []
+    comment_page_limit = 1
+    for i in 1:comment_page_limit
+        all_reviews = []
+        try
+            html = get_html(url * "reviews/page/" * string(i))
+            all_reviews = html.find_all("li", class_="film-detail")
+            # print("\n$all_reviews\n")
+        catch e
+            print("\nERROR 1:\n$e\n")
+            continue
+        end
+        for review in all_reviews
+            try                
+                review_text = "";
+                buffer_review = review.find("div", class_="body-text -prose collapsible-text")
+                if !isnothing(buffer_review.find("p", class_="contains-spoilers"))
+                    review_text = buffer_review.find("div", "hidden-spoilers expanded-text").text
+                else
+                    review_text = buffer_review.text
+                end
+                push!(movie_reviews, review_text)
+            catch e
+                print("\nERROR 2:\n$e\n")
+            end
+        end
+    end
+    return movie_reviews
+end
+
+
+function metric_converter(metric::String)
+
+    number = nothing;
+    try
+        metric = lowercase(metric)
+        multipliers = Dict("k" => 1e3, "m" => 1e6, "b" => 1e9)
+
+        suffix = str[end]
+        number_str = str[1:end-1]
+
+        number = parse(Float64, number_str)
+
+        if haskey(multipliers, suffix)
+            number *= multipliers[suffix]
+        end
+    catch e
+        print("\nERROR METRIC CONVERTER:\n$e\n")
+    end
+
+    return number
+end
+
+
+function insert(contents::Array)
+    try
+        if length(contents) > 0
+            # println("")
+            # insert_many_contents(Mongo("localhost", 27017), "letterbox_data", "movies", contents)
+            insert_many_contents("localhost", 27017, "letterbox_data", "movies", contents)
+        end
+    catch e
+        print("\nERROR:\n$e\n")
+    end
+end
+
+
+function get_html(url::String)
+
+    tries = 0
+    headers = Dict("User-Agent"=> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
+
+    while tries < 5
+        try
+            response = HTTP.request("GET", url, headers)
+            body = String(response.body)
+            html = bs4(body, features="html.parser")
+
+            return html
+        catch
+            tries += 1
+        end
+    end
+end
+
+
+function main()
+    process_scraping(content_type, base_url)
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
---
+++ prepare_data/prepare_data.jl
@@ -0,0 +1,450 @@
+include("../BBDD/mongo_connection.jl")
+using DataFrames
+# import DataFrames: aggregate, Aggregate
+import Tables: rows
+using CSV
+using Dates
+
+
+function log(x) println("$(now()) | $x") end
+
+csv_filename = "tp_data_final.csv"
+
+db_testing = "testing"
+
+col_imdb_complete = "imdb_complete"
+col_tmdb_complete = "tmdb_complete"
+col_comments_rating = "collaborative_db"
+col_titlebasics = "imdbTitleBasics"
+col_titleratings = "imdbTitleRatings"
+col_namebasics = "imdbNameBasics"
+col_datatp = "data_tp_final"
+
+imdbs_list = [
+    "tt10133702",
+    "tt10333266",
+    "tt10350420",
+    "tt10399902",
+    "tt10402396",
+    "tt10469410",
+    "tt10712472",
+    "tt11244166",
+    "tt11257606",
+    "tt11281192",
+    ]
+    
+mongo = Mongo("localhost", 27017)
+
+
+function get_data_from_mongo(database::String, collection::String, query::Dict, projection::Dict)
+
+    # log("Searching data in '$database.$collection'.")
+    
+    pipeline = [
+        Dict( "\$match" => query ),
+        Dict( "\$count" => "count" )
+    ]
+    
+    result_count = run_aggregate(mongo, database, collection, pipeline)
+    if length(result_count) == 0
+        log("\tNo items found in '$database.$collection'.\n")
+        return DataFrame
+    end
+    total_items = result_count[1]["count"]
+
+    step = 1000000
+    # log("\tThere are $total_items items to get.")
+
+    n_got = 0
+    df_items = DataFrame()
+
+    for start in 0 : step : total_items+1
+        pipeline = [
+            Dict( "\$match" => query ),
+            Dict( "\$skip" => start ),
+            Dict( "\$limit" => step ),
+            Dict( "\$project" => projection ),
+        ]
+        
+        items = run_aggregate(mongo, database, collection, pipeline)
+        
+        df_current = DataFrame(items)
+        # df_current = vcat(DataFrame.(items)...)
+        
+        df_items = vcat( df_items, df_current )
+        
+        n_got += length(items)
+        # log("\t\t$n_got / $total_items")
+    end
+    
+    if "imdbId" in names(df_items)
+        dropmissing!(df_items, :imdbId);
+    end
+
+    for col_name in names(df_items)
+        try
+            df_items[:, col_name] = replace.(df_items[:, col_name], "\\N" => nothing)
+            df_items[:, col_name] = replace.(df_items[:, col_name], "\\\\N" => nothing)
+        catch e
+            nothing
+        end
+    end
+
+    # log("\t$(nrow(df_items)) total items got.\n")
+    return df_items
+
+end
+
+
+# query_imdbs = Dict()
+query_imdbs = Dict( "imdbId" => Dict( "\$in" => imdbs_list ) )
+
+
+# #### Imdb TitleBasics data
+function get_title_basics()
+    #=
+        Returns DataFrame with columns: imdbId, type, year, duration, genres
+    =#
+    # query = Dict()
+    query = Dict( "imdbId" => Dict( "\$in" => imdbs_list ) )
+    projection = Dict(
+        "_id" => 0,
+        "imdbId" => "\$imdbId",
+        "type" => "\$titleType",
+        "year" => "\$startYear",
+        "duration" => "\$runtimeMinutes",
+        "genres" => "\$genres",
+    )
+    df_titlebasics = get_data_from_mongo(db_testing, col_titlebasics, query, projection)
+    transform!(df_titlebasics, :genres .=> ByRow(x -> [ String(i) for i in split(x, ",") ]) .=> :genres)
+    log("$(nrow(df_titlebasics)) items from $col_titlebasics ") # \n$(first(df_titlebasics, 2))\n")
+
+    # log("\n\n$df_titlebasics\n\n")
+    return df_titlebasics
+end
+
+
+# #### Imdb TitleRatings data
+function get_title_ratings(df::DataFrame)
+    #=
+        Arguments
+            df (DataFrame) with columns (at least) imdbId
+        Returns with original dataframe with added columns rating, votes
+    =#
+    # query = Dict()
+    query = Dict( "imdbId" => Dict( "\$in" => imdbs_list ) )
+    projection = Dict(
+        "_id" => 0,
+        "imdbId" => "\$imdbId",
+        "rating" => "\$averageRating",
+        "votes" => "\$numVotes",
+    )
+    df_titleratings = get_data_from_mongo(db_testing, col_titleratings, query, projection)
+    log("$(nrow(df_titleratings)) items from $col_titleratings ") # \n$(first(df_titleratings, 2))\n")
+    df = leftjoin(df, df_titleratings, on=:imdbId) # Add columns rating, votes
+    df_titleratings = nothing
+    # log("\t -->> $(nrow(df)) ITEMS <<--\n")
+
+    # log("\n\n$df\n\n")
+    return df
+end
+
+
+# #### Letterboxd reviews data 
+# Buscar ultima fecha (createdAt) y filtrar asi
+function get_reviews(df::DataFrame)
+    #=
+        Arguments
+            df (DataFrame) with columns (at least) imdbId
+        Returns with original dataframe with added column reviews
+    =#
+    query = Dict()
+    query = Dict( "imdb_id" => Dict( "\$in" => imdbs_list ) )
+    projection = Dict(
+        "_id" => 0,
+        "imdbId" => "\$imdb_id",
+        "reviews" => "\$reviews_analysis.compound",
+    )
+    df_collaborativedb = get_data_from_mongo(db_testing, col_comments_rating, query, projection)
+    df_collaborativedb = combine(groupby(df_collaborativedb, :imdbId), :reviews => maximum => :reviews)
+    log("$(nrow(df_collaborativedb)) items from $col_comments_rating ") # \n$(first(df_collaborativedb, 2))\n")
+    df = leftjoin(df, df_collaborativedb, on=:imdbId)
+    df_collaborativedb = nothing
+    # log("\t -->> $(nrow(df)) ITEMS <<--\n")
+
+    # log("\n\n$df\n\n")
+    return df
+end
+
+
+# #### Imdb NameBasics data (directors)
+function get_name_basics(df::DataFrame)
+    #=
+        Arguments
+            df (DataFrame) with columns (at least) imdbId
+        Returns with original dataframe with added column directors
+    =#
+    query = Dict(
+        "\$or" => [ Dict( "knownForTitles" => Dict( "\$regex" => value ) ) for value in imdbs_list ]
+    )
+    projection = Dict(
+        "_id" => 0,
+        "profession" => "\$primaryProfession",
+        "name" => "\$primaryName",
+        "titles" => "\$knownForTitles"
+    )
+    
+    # df_namebasics = get_data_from_mongo(db_testing, col_namebasics, query, projection)
+    # CSV.write("names_test.csv", df_namebasics)
+    df_namebasics = DataFrame( CSV.read("names_test.csv", DataFrame) )
+    
+    df_namebasics = filter(row -> occursin( "director" , row.profession ) , df_namebasics)
+    df_namebasics = df_namebasics[ : , [:name, :titles] ]
+    transform!(df_namebasics, :titles .=> ByRow(x -> [ String(i) for i in split(x, ",") ]) .=> :titles)
+    df_namebasics = DataFrame( [ ( name=row["name"], imdbId=val ) for row in eachrow(df_namebasics) for val in row[:titles] ] )
+    df_namebasics = filter(row -> row.imdbId in imdbs_list , df_namebasics)
+    df_directors = DataFrame()
+    for i in groupby( df_namebasics, :imdbId )
+        names_vector = [ String(_name) for _name in i[:, :name] ]
+        imdb = i[1,"imdbId"]
+        push!(df_directors, ( directors=names_vector, imdbId=imdb ))
+    end
+    df = leftjoin(df, df_directors, on=:imdbId)
+    df_namebasics = nothing
+    df_directors = nothing
+    # log("\t -->> $(nrow(df)) ITEMS <<--\n")
+
+    # log("\n\n$df\n\n")
+    return df
+end
+
+
+function isnull(value)
+    if ismissing(value) || value===nothing || value==""
+        return true
+    end
+    return false
+end
+
+
+function set_company_names(value)
+    if value === nothing
+        return nothing
+    end
+    companies = String[]
+    for i in value
+        push!(companies, String(i["Name"]))
+    end
+    return companies
+end
+
+
+function set_country(value)
+    if value === nothing
+        return nothing
+    end
+    return value[1]
+end
+
+
+function set_string_array(value)
+    if value === nothing
+        return nothing
+    end
+    values_list = String[]
+    for i in value
+        push!(values_list, String(i))
+    end
+    return values_list
+end
+
+
+# Imdb complete data 
+function get_imdb_complete(df::DataFrame)
+    #=
+        Arguments
+            df (DataFrame) with columns (at least) imdbId
+        Returns with original dataframe with added columns cast, country, releaseDate, keywords, companies
+    =#
+    # query = Dict()
+    query = Dict( "Id" => Dict( "\$in" => imdbs_list ) )
+    projection = Dict(
+        "_id" => 0,
+        "imdbId" => "\$Id",
+        "cast" => "\$Cast",
+        "country" => "\$Country",
+        "releaseDate" => "\$ReleaseDate",
+        "keywords" => "\$Keywords",
+        "companies" => "\$Companies",
+    )
+    df_imdb_complete = get_data_from_mongo(db_testing, col_imdb_complete, query, projection)
+
+    df_imdb_complete = transform(df_imdb_complete, :companies .=> ByRow(x -> set_company_names(x)) .=> :companies)
+    df_imdb_complete = transform(df_imdb_complete, :country .=> ByRow(x -> set_country(x)) .=> :country)
+
+    for col_name in [ "cast", "keywords" ]
+        df_imdb_complete = transform(df_imdb_complete, col_name .=> ByRow(x -> set_string_array(x)) .=> col_name)
+    end
+
+    log("$(nrow(df_imdb_complete)) items from $col_imdb_complete ") # \n$(first(df_imdb_complete, 2))\n")
+
+    df = leftjoin(df, df_imdb_complete, on=:imdbId) # Add columns: cast, country, releaseDate, keywords, companies
+
+    # log("\t -->> $(nrow(df)) ITEMS <<--\n")
+    df_imdb_complete = nothing
+
+    # log("\n\n$df\n\n")
+    return df
+end
+
+
+# Tmdb complete data
+function get_tmdb_complete()
+    # query = Dict( "imdbId" => Dict( "\$ne" => nothing ) )
+    query = Dict( "imdbId" => Dict( "\$in" => imdbs_list ) )
+    projection = Dict(
+        "_id" => 0,
+        "imdbId" => 1,
+        "releaseDate" => "\$ReleaseDate",
+        "votes" => "\$vote_count",
+        "rating" => "\$vote_average",
+        "duration" => "\$Duration",
+        "genres" => "\$Genres",
+        "country" => "\$Country",
+        "keywords" => "\$Keywords",
+        "cast" => "\$Cast",
+        "companies" => "\$Companies",
+        "directors" => "\$Directors"
+    )
+    df_tmdb = get_data_from_mongo(db_testing, col_tmdb_complete, query, projection)
+
+    df_tmdb = unique(df_tmdb, :imdbId)
+
+    df_tmdb = transform(df_tmdb, :companies .=> ByRow(x -> set_company_names(x)) .=> :companies)
+    df_tmdb = transform(df_tmdb, :country .=> ByRow(x -> set_country(x)) .=> :country)
+    for col_name in [ "genres", "companies", "cast", "directors", "keywords" ]
+        df_tmdb = transform(df_tmdb, col_name .=> ByRow(x -> set_string_array(x)) .=> col_name)
+    end
+
+    log("$(nrow(df_tmdb)) items from $col_tmdb_complete ") # \n$(first(df_tmdb, 2))\n")
+
+    return df_tmdb
+end
+
+
+function complete_data(df, df_tmdb)
+
+    cols = names(df)
+
+    log("Completing data for $(cols) rows:\n")
+
+    fields = [
+		"releaseDate",
+		"votes",
+		"rating",
+		"duration",
+        "genres",
+        "country",
+        "keywords",
+        "cast",
+        "companies",
+        "directors",
+    ]
+
+    index = 0
+    for field in fields
+        index += 1
+        # log("\tField '$field' $(index)/$(length(fields))")
+        # log("$(names(df))")
+
+        # log("\t\tGetting null data from df")
+        null_data = df[ [  isnull(i) for i in df[:, field] ] , : ]
+        if isempty(null_data)
+            log("\t\t\tNo null data")
+            continue
+        end
+
+        # log("\t\tGetting not null data from df")
+        not_null_data = df[ [  !(isnull(i)) for i in df[:,field] ] , : ]
+
+        df_tmdb_info = df_tmdb[ : , ["imdbId", field] ]
+        # log("\t\tGetting not null data from tmdb df")
+        df_tmdb_info = df_tmdb_info[ [  !(isnull(i)) for i in df_tmdb_info[:,field] ] , : ]
+
+        # log("\t\tRemoving field $field from df")
+        select!(null_data, Not([field]))
+        
+        # log("\t\tJoin data")
+        null_data = leftjoin(null_data, df_tmdb_info, on=:imdbId)
+
+        # log("\t\tConcat data")
+        df = vcat( null_data[:, cols] , not_null_data[:, cols] )
+
+        # println("\n\n")
+    end
+	
+    return df
+end
+
+
+function insert_to_mongo(df)
+
+    log("Inserting data: $(nrow(df)) rows:\n")
+    items = rows(df) |> collect
+
+    items_dict = []
+    for item in items
+        a = Dict(names(item) .=> values(item))
+        # print(a)
+        push!(items_dict, a)
+    end
+
+    log("\tInserting to mongo...")
+
+    # insert_many_contents(mongo, db_testing, col_datatp, items_dict)
+    for item in items_dict
+        try
+            insert_content(mongo, db_testing, col_datatp, item)
+        catch e
+            println("\n\nERROR: $e \n$item\n\n")
+            break
+        end
+    end
+end
+
+
+function main()
+
+    # df = DataFrame()
+|
+    df = get_title_basics()
+    df = get_title_ratings(df)
+    df = get_reviews(df)
+    df = get_name_basics(df)
+    df = get_imdb_complete(df)
+    df_tmdb = get_tmdb_complete()
+    
+    # CSV.write("out.csv", df, transform=(col, val) -> something(val, missing))
+    # CSV.write("out2.csv", df_tmdb, transform=(col, val) -> something(val, missing))
+    
+    df = complete_data(df, df_tmdb)
+
+    # df = df.fillna(value=nan).replace(to_replace=nan, value=None)
+    # df = df.replace(to_replace="", value=nothing)
+    # df = df.replace(to_replace="\\N", value=nothing)
+    # df = df.replace(to_replace="\\\\N", value=nothing)
+
+    # CSV.write("out3.csv", df, transform=(col, val) -> something(val, missing))
+
+    insert_to_mongo(df)
+
+    df = df[ [  !(isnull.(i)) for i in df[:, :rating] ] , : ]
+    CSV.write(csv_filename, df)
+
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
+
