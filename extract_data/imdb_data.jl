3edf6369649cbad74d1a0f83dd6887e34aef64a4
Dolores Pavon <dolorespavon01@gmail.com>
2024-06-30 21:41:04 -0300

    Refactor : CSV Upload

--- extract_data/imdb_data.jl
+++ extract_data/imdb_data.jl
@@ -131,12 +131,12 @@ function main()
 
         imdb_files = [
             "https://datasets.imdbws.com/title.ratings.tsv.gz",
-            # "https://datasets.imdbws.com/title.episode.tsv.gz",
-            # "https://datasets.imdbws.com/title.basics.tsv.gz",
-            # "https://datasets.imdbws.com/title.crew.tsv.gz",
-            # "https://datasets.imdbws.com/name.basics.tsv.gz",
-            # "https://datasets.imdbws.com/title.akas.tsv.gz",
-            # "https://datasets.imdbws.com/title.principals.tsv.gz"
+            "https://datasets.imdbws.com/title.episode.tsv.gz",
+            "https://datasets.imdbws.com/title.basics.tsv.gz",
+            "https://datasets.imdbws.com/title.crew.tsv.gz",
+            "https://datasets.imdbws.com/name.basics.tsv.gz",
+            "https://datasets.imdbws.com/title.akas.tsv.gz",
+            "https://datasets.imdbws.com/title.principals.tsv.gz"
         ]
 
         host = "localhost"
